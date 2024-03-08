4ff893f19ec10d08fae66b0eaea872e6a877ce01
Fábio Alves <fabioalves.to@gmail.com>
2024-03-08 10:25:13 -0300

    Adicionando link para portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Provider } from "react-redux";
 import { persistor, store } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Routers from "./routers";
+import * as Icons from "@heroicons/react/24/solid";
 
 function App() {
   return (
@@ -11,6 +12,16 @@ function App() {
         <PersistGate loading={null} persistor={persistor}>
           <Routers />
         </PersistGate>
+
+        <div className="fixed bottom-6 right-6">
+          <a
+            href="https://fabioalves.site/"
+            title="Desenvolvido por Fábio Alves"
+            target="_blank"
+          >
+            <Icons.FireIcon className="h-8 w-8 fill-emerald-300 transition-all hover:scale-150 cursor-pointer" />
+          </a>
+        </div>
       </Provider>
     </>
   );
